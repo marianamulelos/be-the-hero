44c7a3e45bbb566bed5949ef103d10d7755e3869
Mariana Mulelos <marianamulelos@hotmail.com>
2020-03-27 20:30:10 -0300

    Back-end da aplicação

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mariana Mulelos
         </p>
         <a
           className="App-link"
